fc9f569a7003beb02270aac59779c567e23b1f01
ryangsangalang <ryan.sangalang@icloud.com>
2026-04-27 17:11:18 -0700

    Update ex04.js

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,5 +1,25 @@
+
 let count = 0;
+let colors = ["Orchid", "Coral", "HotPink", "Plum", "Pink"];
+let colorCount;
+
+let cat={
+    color: "gray",
+    isCute: true,
+};
+
 $("#needy-button").click(function () {
-    count = count + 3;
-    $("#needy-button").html("Wow....you clicked me " + count + " times.....");
+
+    count = count + 1;
+
+    colorCount = count - 1;
+
+    $("#needy-button").html("Wow....you clicked me " + count + " times..... " + colors[count]);
+
+    $("#needy-button").css("background-color", colors[colorCount]);
+
+    $("html").css("background-color", colors[colorCount+1]);
+
+    console.log(cat.color);
+
 });
