981bc4474e63715433ad4293b1598e5554d22605
ryangsangalang <ryan.sangalang@icloud.com>
2026-05-19 13:07:18 -0700

    fdafd

--- exercises/garden/creature.js
+++ exercises/garden/creature.js
@@ -1,37 +1,108 @@
-// add creatuer to da gardenn
-$("#crAdd").click(function () {
+// store all creatures in an array
 
-    // grab the value from the text input and assign it to a variable crName
-    let crName = $("#crName").val();
-    let crColor = $("#crColor").val();
-    let crEyesNum = $("#crEyesNum").val();
-    let crPupilColor = $("#crPupilColor").val();
-    // lets construct html for eyes
-    let crEyesHtml = "";
-    for (let i = 0; i < crEyesNum; i++) {
-        crEyesHtml = crEyesHtml + `<div class='eye' style='color: ${crPupilColor}'>.</div>`;
+let allCreatures=[];
 
+
+// functions
+// function to grab data from the form
+function getCreatureFromForm(){
+  
+  const freshCreature={
+        name: $("#crName").val(),
+        color: $("#crColor").val(),
+        eyesNum: $("#crEyesNum").val()
+    };
+
+return freshCreature;
+};
+
+// the checks function
+function isCreatureValid(creature) {
+  if (creature.name === "" ) return false;
+  if (creature.name.length >12) return false;
+  if ( isNaN(creature.eyesNum) || creature.eyesNum >5 ) return false;
+  return true;
+}
+
+// functions add creature to the page
+function renderCreature(creature) {
+
+    let crEyesHtml="";
+    for (let i = 0; i < creature.eyesNum; i++) {
+        crEyesHtml=crEyesHtml+ "<div class='eye'>.</div>";
     }
-    // test in console
-    console.log(crName);
-    console.log(crColor);
-    console.log(crEyesNum);
-    console.log(crPupilColor);
 
-    if (crName.length > 2) {
-        $("#creature-list").append(`
-            <div class="creature">
-                <div class="creature-body" style="background-color: ${crColor}"> ${crEyesHtml} </div>
-                <div class="creature-info">${crName}</div>
-            </div>
-        `);
+    const html=`
+        <div class="creature">
+            <div class="creature-body" style="background-color: ${creature.color}"> ${crEyesHtml} </div>
+            <div class="creature-info">${creature.name}</div>
+        </div>
+    `;
+
+    return html;
+}
+
+// append creature to the page
+function addCreatureToDOM(creature) {
+    const html=renderCreature(creature);
+    $("#creature-list").append(html);
+}
+
+
+
+// the main brain
+$("#crAdd").click( function(){
+
+    // create creature object from the form inputs
+    const newCreature = getCreatureFromForm();
+    console.log(newCreature);
+  
+  // safety checks
+    console.log( isCreatureValid(newCreature) );
+    if ( isCreatureValid(newCreature)==false ) { // true==false 
+     return; // stops the function which is "click" one
     }
-    //"<div>" + crName + crColor + crEyesNum + "</div>");
 
-    $("#crName").val("");
-    $("#crColor").val("");
-    $("#crEyesNum").val("");
-    $("#crPupilColor").val("");
+    // add creature to the page
+    addCreatureToDOM(newCreature)
+
+    // save to the memory 
+    allCreatures.push(newCreature);
+    // reset the fporm prepare for the next iteration
 
 });
+
+// grab the value from the text input and assign it to a variable crName
+//     let crName = $("#crName").val();
+//     let crColor = $("#crColor").val();
+//     let crEyesNum = $("#crEyesNum").val();
+//     let crPupilColor = $("#crPupilColor").val();
+//     // lets construct html for eyes
+//     let crEyesHtml = "";
+//     for (let i = 0; i < crEyesNum; i++) {
+//         crEyesHtml = crEyesHtml + `<div class='eye' style='color: ${crPupilColor}'>.</div>`;
+
+//     }
+//     // test in console
+//     console.log(crName);
+//     console.log(crColor);
+//     console.log(crEyesNum);
+//     console.log(crPupilColor);
+
+//     if (crName.length > 2) {
+//         $("#creature-list").append(`
+//             <div class="creature">
+//                 <div class="creature-body" style="background-color: ${crColor}"> ${crEyesHtml} </div>
+//                 <div class="creature-info">${crName}</div>
+//             </div>
+//         `);
+//     }
+//     //"<div>" + crName + crColor + crEyesNum + "</div>");
+
+//     $("#crName").val("");
+//     $("#crColor").val("");
+//     $("#crEyesNum").val("");
+//     $("#crPupilColor").val("");
+
+// });
 
