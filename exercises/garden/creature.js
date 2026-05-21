d1a92f4fb602da2fca0c365e45faec32447b2e38
ryangsangalang <ryan.sangalang@icloud.com>
2026-05-21 13:04:33 -0700

    he,p me

--- exercises/garden/creature.js
+++ exercises/garden/creature.js
@@ -1,38 +1,71 @@
 // store all creatures in an array
-
-let allCreatures=[];
-
+let allCreatures = [];
 
 // functions
 // function to grab data from the form
-function getCreatureFromForm(){
-  
-  const freshCreature={
+function getCreatureFromForm() {
+
+    const freshCreature = {
         name: $("#crName").val(),
         color: $("#crColor").val(),
         eyesNum: $("#crEyesNum").val()
     };
 
-return freshCreature;
+    return freshCreature;
 };
+
+async function getRandomName() {
+    // goes and grabs some data from an api
+    const response = await fetch("https://api.gofakeit.com/funcs/petname", { method: "GET" });
+    // converts the response to plain text
+    const nameRandom = await response.text();
+    console.log("Got name:", nameRandom);
+    return nameRandom;
+}
+
+// random creature
+
+async function randomizeCreature() {
+
+    const eyesRandom = Math.floor(Math.random() * 5) + 1; // 0 to 5
+    const nameRandom = await getRandomName();
+    const colorRandom = await getRandomColor();
+
+    const randomCreature = {
+        name: nameRandom,
+        color: colorRandom,
+        eyesNum: eyesRandom
+    };
+    return randomCreature;
+
+}
+
+async function getRandomColor() {
+    // goes and grabs some data from an api
+    const response = await fetch("https://api.gofakeit.com/funcs/hexcolor", { method: "GET" });
+    // converts the response to plain text
+    const nameRandom = await response.text();
+    console.log("Got color:", nameRandom);
+    return nameRandom;
+}
 
 // the checks function
 function isCreatureValid(creature) {
-  if (creature.name === "" ) return false;
-  if (creature.name.length >12) return false;
-  if ( isNaN(creature.eyesNum) || creature.eyesNum >5 ) return false;
-  return true;
+    if (creature.name === "") return false;
+    if (creature.name.length > 12) return false;
+    if (isNaN(creature.eyesNum) || creature.eyesNum > 5) return false;
+    return true;
 }
 
 // functions add creature to the page
 function renderCreature(creature) {
 
-    let crEyesHtml="";
+    let crEyesHtml = "";
     for (let i = 0; i < creature.eyesNum; i++) {
-        crEyesHtml=crEyesHtml+ "<div class='eye'>.</div>";
+        crEyesHtml = crEyesHtml + "<div class='eye'>.</div>";
     }
 
-    const html=`
+    const html = `
         <div class="creature">
             <div class="creature-body" style="background-color: ${creature.color}"> ${crEyesHtml} </div>
             <div class="creature-info">${creature.name}</div>
@@ -44,65 +77,38 @@ function renderCreature(creature) {
 
 // append creature to the page
 function addCreatureToDOM(creature) {
-    const html=renderCreature(creature);
+    const html = renderCreature(creature);
     $("#creature-list").append(html);
 }
 
-
-
 // the main brain
-$("#crAdd").click( function(){
+$("#crAdd").click(async function () {
 
-    // create creature object from the form inputs
-    const newCreature = getCreatureFromForm();
+    let newCreature;
+    // choose the way /random or manual
+    // if checked go random mode
+    if ($("#crRandom").is(":checked")) {
+        newCreature = await randomizeCreature();
+        console.log("random way");
+    }
+    // if not checked go manual mode
+    else { newCreature = getCreatureFromForm(); console.log("manual way"); }
+
+
     console.log(newCreature);
-  
-  // safety checks
-    console.log( isCreatureValid(newCreature) );
-    if ( isCreatureValid(newCreature)==false ) { // true==false 
-     return; // stops the function which is "click" one
+
+    // safety checks
+    console.log(isCreatureValid(newCreature));
+    if (isCreatureValid(newCreature) == false) { // true==false 
+        return; // stops the function which is "click" one
     }
 
     // add creature to the page
     addCreatureToDOM(newCreature)
 
-    // save to the memory 
+    // save creature to the memory 
     allCreatures.push(newCreature);
+
     // reset the fporm prepare for the next iteration
 
 });
-
-// grab the value from the text input and assign it to a variable crName
-//     let crName = $("#crName").val();
-//     let crColor = $("#crColor").val();
-//     let crEyesNum = $("#crEyesNum").val();
-//     let crPupilColor = $("#crPupilColor").val();
-//     // lets construct html for eyes
-//     let crEyesHtml = "";
-//     for (let i = 0; i < crEyesNum; i++) {
-//         crEyesHtml = crEyesHtml + `<div class='eye' style='color: ${crPupilColor}'>.</div>`;
-
-//     }
-//     // test in console
-//     console.log(crName);
-//     console.log(crColor);
-//     console.log(crEyesNum);
-//     console.log(crPupilColor);
-
-//     if (crName.length > 2) {
-//         $("#creature-list").append(`
-//             <div class="creature">
-//                 <div class="creature-body" style="background-color: ${crColor}"> ${crEyesHtml} </div>
-//                 <div class="creature-info">${crName}</div>
-//             </div>
-//         `);
-//     }
-//     //"<div>" + crName + crColor + crEyesNum + "</div>");
-
-//     $("#crName").val("");
-//     $("#crColor").val("");
-//     $("#crEyesNum").val("");
-//     $("#crPupilColor").val("");
-
-// });
-
